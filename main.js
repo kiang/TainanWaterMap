f93012b0e9207194ae18f598ecf09234626904db
kiang <kiange@gmail.com>
2016-06-13 17:23:10 +0800

    basic view for alert level of points

---
+++ main.js
@@ -0,0 +1,76 @@
+/*
+ * 流域編號=1580(八掌溪流域)、1590(急水溪流域)、1600(將軍溪流域)、1630(曾文溪流域)、1650(鹽水溪流域)、1660(二仁溪流域)
+ */
+var map, info, bounds, points;
+var rivers = ['1580', '1590', '1600', '1630', '1650', '1660'];
+function initialize() {
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+        zoom: 12,
+        center: {lat: 22.672925, lng: 120.309465}
+    });
+    info = new google.maps.InfoWindow();
+    bounds = new google.maps.LatLngBounds();
+
+    $.getJSON('points.json', {}, function (r) {
+        points = r;
+        for (k in rivers) {
+            $.getJSON('http://i.tainan.gov.tw/TainanLocalWst.php?basin=' + rivers[k], {}, function (i) {
+                for (g in i.local_wst_warn_info) {
+                    var key = i.local_wst_warn_info[g].st_no;
+                    points[key].alert_level = i.local_wst_warn_info[g].alert_level;
+                    points[key].change = i.local_wst_warn_info[g].change;
+                    points[key].date = i.local_wst_warn_info[g].date;
+                    points[key].info = i.local_wst_warn_info[g].info;
+                    points[key].warn_info = i.local_wst_warn_info[g].warn_info;
+                    var geoPoint = (new google.maps.LatLng(points[key].latitude, points[key].longitude));
+
+                    if ('未達警戒' === points[key].alert_level) {
+                        var marker = new google.maps.Marker({
+                            position: geoPoint,
+                            map: map,
+                            icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+                            title: points[key].name
+                        });
+                    } else {
+                        var marker = new google.maps.Marker({
+                            position: geoPoint,
+                            map: map,
+                            icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+                            title: points[key].name
+                        });
+                    }
+
+
+                    marker.data = points[key];
+                    marker.addListener('click', function () {
+                        var infoText = '<strong>' + this.data.name + '</strong>';
+                        infoText += '<br />站碼: ' + this.data.code;
+                        infoText += '<br />河川分區: ' + this.data.type;
+                        infoText += '<br />行政區: ' + this.data.area;
+                        infoText += '<br />轄管單位: ' + this.data.admin;
+                        infoText += '<br />河川排水: ' + this.data.river;
+                        infoText += '<br />警戒狀態: ' + this.data.alert_level;
+                        infoText += '<br />說明: ' + this.data.warn_info;
+                        info.setContent(infoText);
+                        info.open(map, this);
+                        map.setZoom(15);
+                        map.setCenter(this.getPosition());
+                        $('#pointContent').html(infoText);
+                    });
+                    bounds.extend(geoPoint);
+                }
+            });
+        }
+        setTimeout(function () {
+            map.fitBounds(bounds);
+        }, 2000);
+    });
+
+    $('a.bounds-reset').click(function () {
+        map.fitBounds(bounds);
+        return false;
+    });
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
