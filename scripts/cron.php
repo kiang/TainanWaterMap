d8f63b080e4d7ded1182d30c1274368d71c553e4
kiang <kiange@gmail.com>
2017-08-02 10:18:20 +0800

    pretty in json files

--- scripts/cron.php
+++ scripts/cron.php
@@ -11,7 +11,7 @@ exec("cd {$rootPath} && /usr/bin/git pull");
 
 $rivers = array('1580', '1590', '1600', '1630', '1650', '1660');
 foreach($rivers AS $river) {
-  file_put_contents($jsonPath . '/' . $river . '.json', file_get_contents('http://210.61.23.112/tainanwatermobile/TainanLocalWst/GetLocalWstWarnInfo.aspx?basin=' . $river));
+  file_put_contents($jsonPath . '/' . $river . '.json', json_encode(json_decode(file_get_contents('http://210.61.23.112/tainanwatermobile/TainanLocalWst/GetLocalWstWarnInfo.aspx?basin=' . $river)), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 }
 
 exec("cd {$rootPath} && /usr/bin/git add -A");
